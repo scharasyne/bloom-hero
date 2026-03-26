040b898aac061f5f272cd0b058631d66d7383659
jomacsolz <jomacsolz@gmail.com>
2026-03-26 12:36:50 +0800

    added temporary button for vendor application

--- bloom-hero/src/app/customer/dashboard/page.tsx
+++ bloom-hero/src/app/customer/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import NavBar from "@/components/navbar";
 import { createSupabaseServerClient } from "@/lib/supabase/server-client";
 import { redirect } from "next/navigation";
-import ApplyVendorModal from "./ApplyVendorModal";
 
 export default async function CustomerDashboardPage() {
   const supabase = await createSupabaseServerClient();
@@ -38,7 +37,12 @@ export default async function CustomerDashboardPage() {
         >
           View Order History
         </a>
-        <ApplyVendorModal />
+        <a
+          href="/customer/vendor-application"
+          className="inline-block bg-[#d24b46] text-white px-4 py-2 rounded hover:bg-[#bb3f3a]"
+        >
+          Apply as Vendor
+        </a>
       </div>
       <hr />
       <p>If you can see this page, routing is working correctly.</p>
